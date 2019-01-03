34aa52ba83fcb36790437681441dc5b3a165e16e
abousquet <bousquet@vt.edu>
2019-01-02 22:26:05 -0500

    Using two simple strategies to make assignments; Added a grid_str function to pretty print grids; added a few more tests;
    The two simple strategies are as follows:
    - Assign a value if it is the only possible value for that cell
    - Assign a value if it is the only cell in its column/row/square that can be
      assigned that value

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,22 @@
 
+def grid_str(grid):
+    square_len = int(pow(len(grid), 0.5))
+    row_strings = []
+    for row in grid:
+        tmp_strings = []
+        for i in range(0, len(row), square_len):
+            tmp_strings.append(" | ".join([str(c) if c is
+                               not None else ' ' for c in
+                               row[i:i+square_len]]))
+        row_strings.append(" || ".join(tmp_strings))
+    sep = "\n" + ("-" * len(row_strings[0])) + "\n"
+    third_sep = "\n" + ("=" * len(row_strings[0])) + "\n"
+    tmp_rows = []
+    for i in range(0, len(row_strings), square_len):
+        tmp_rows.append(sep.join(row_strings[i:i+square_len]))
+    return third_sep.join(tmp_rows)
+
+
 def is_solved(grid):
     """
     This functions works by making all rows and columns into sets and then
@@ -8,7 +26,9 @@ def is_solved(grid):
     are all unique, then squares will be unique as well.
 
     Parameters:
-        grid : NxN list of lists that contains numerical values and None
+        grid : NxN list of lists that contains int and None values
+
+    Returns True if the grid is solved, False otherwise
     """
     # Create a complete set of all values that should be in each row, col, or s
     # square
@@ -25,3 +45,149 @@ def is_solved(grid):
             return False
 
     return True
+
+
+def csp_search(grid):
+    """
+    This function will solve the puzzle using the func parameter to create
+    values.
+
+    Parameters:
+        grid : NxN list of lists that contains int and None values
+
+    Yields an assignment tuple of the form (row, column, assignment)
+
+    Note: Constraint Satisfaction problems can be solved by eliminating the
+    domains of values based on the constraints of the problem. Sudoku's
+    constraints are that each value assignment must be unique to its row,
+    column, and square. Many of these problems can be solved by simple domain
+    reduction, but some values must be deduced by inference of the constraints
+    of its neighbors.
+    For example: The left-most square is missing {2, 4, 8}. By reducing each of
+    their domains, we get their domains as {2, 4}, {2, 4, 8}, and {2, 4}, from
+    top to bottom. We know that the square is missing an 8 and the only
+    position that can still take that value is the middle one of the left
+    square.
+        _______________________________________
+        | 1 |   | 3 ||   | 5 | 6 || 7 | 8 | 9 |
+        | 7 |   | 9 || 1 |   | 3 ||   | 5 | 6 |
+        |   | 5 | 6 || 7 | 8 | 9 || 1 |   | 3 |
+        _______________________________________
+    """
+    def square_index(row, col):
+        return ((row // square_len) * square_len) + col // square_len
+
+    def gen_row_col_in_square(index):
+        row_range = (index // square_len) * square_len
+        col_range = (index % square_len) * square_len
+        for r in range(row_range, row_range + square_len):
+            for c in range(col_range, col_range + square_len):
+                yield r, c
+
+    def gen_row_col_in_row(row):
+        for col in range(0, len(grid)):
+            yield row, col
+
+    def gen_row_col_in_col(col):
+        for row in range(0, len(grid)):
+            yield row, col
+
+    def get_possibilities(row, col):
+        sq_index = square_index(row, col)
+        return row_sets[row] & col_sets[col] & square_sets[sq_index]
+
+    def remove_value_from_sets(row, col, assignment):
+        # Update our missing sets after an assignment
+        row_sets[row].remove(assignment)
+        col_sets[col].remove(assignment)
+        square_sets[sq_index].remove(assignment)
+
+    square_len = int(pow(len(grid), 0.5))
+    complete_set = set(range(1, len(grid) + 1))
+    unassigned_locs = set()
+    # Generate requirements sets for all rows
+    row_sets = tuple([complete_set.difference(set(row),
+                     {None}) for row in grid])
+
+    # Generate requirements sets for all columns
+    col_sets = []
+    for i in range(len(grid)):  # col iterator
+        col_set = set()
+        for j in range(len(grid)):  # row iterator
+            if grid[j][i] is None:
+                unassigned_locs.add((j, i))
+            else:
+                col_set.add(grid[j][i])
+        col_sets.append(complete_set.difference(col_set, {None}))
+    col_sets = tuple(col_sets)
+
+    square_sets = []
+    for r in range(0, len(grid), square_len):
+        for c in range(0, len(grid), square_len):
+            square_set = set()
+            for sub_row in range(square_len):
+                square_set.update(grid[r + sub_row][c:c + square_len])
+            square_sets.append(complete_set.difference(square_set, {None}))
+    square_sets = tuple(square_sets)
+
+    assigning_values = True
+    while assigning_values:
+        assigning_values = False
+        for row, col in unassigned_locs:
+            sq_index = square_index(row, col)
+            loc_set = get_possibilities(row, col)
+            if len(loc_set) == 1:
+                assigning_values = True
+                assignment = loc_set.pop()
+                remove_value_from_sets(row, col, assignment)
+                yield row, col, assignment
+                unassigned_locs.remove((row, col))
+                # break to avoid error from changing the set during iteration
+                break
+        if not assigning_values:
+            for row, col in unassigned_locs:
+                sq_index = square_index(row, col)
+                generators = [gen_row_col_in_square(sq_index),
+                              gen_row_col_in_row(row),
+                              gen_row_col_in_col(col)]
+                for generator in generators:
+                    other_union = set()
+                    assigned_set = set()
+                    for other_row, other_col in generator:
+                        if other_row != row or other_col != col:
+                            if grid[other_row][other_col] is None:
+                                other_union |= get_possibilities(other_row,
+                                                                 other_col)
+                            else:
+                                assigned_set.add(grid[other_row][other_col])
+                    possibilities = complete_set - assigned_set - other_union
+                    if len(possibilities) == 1:
+                        assigning_values = True
+                        assignment = possibilities.pop()
+                        remove_value_from_sets(row, col, assignment)
+                        yield row, col, assignment
+                        unassigned_locs.remove((row, col))
+                        break
+                if assigning_values:
+                    break
+
+
+def solve(grid, func=csp_search):
+    """
+    This function will solve the puzzle using the func parameter to create
+    values.
+
+    Parameters:
+        grid : NxN list of lists that contains int and None values
+        func : generator that takes a grid and returns a tuple,
+            (row, col, assigment)
+
+    Returns the solved grid
+    """
+    for row, col, assignment in func(grid):
+        # print (row, col, assignment)
+        grid[row][col] = assignment
+        # print(grid_str(grid))
+        # print()
+
+    return grid
--- test_sudoku.py
+++ test_sudoku.py
@@ -2,10 +2,10 @@
     Unit Tests for Sudoku
 """
 import sudoku
+import unittest
 
 
-class TestSudoku:
-
+class TestSudoku(unittest.TestCase):
     def test_solved(self):
         """
             Test that a solved puzzle will be marked solved
@@ -21,7 +21,7 @@ class TestSudoku:
                 [9, 6, 8, 2, 3, 1, 5, 7, 4],
                 [5, 7, 4, 9, 6, 8, 2, 3, 1]]
 
-        assert sudoku.is_solved(grid)
+        self.assertTrue(sudoku.is_solved(grid))
 
     def test_column_not_solved(self):
         """
@@ -37,7 +37,7 @@ class TestSudoku:
                 [1, 2, 3, 4, 5, 6, 7, 8, 9],
                 [1, 2, 3, 4, 5, 6, 7, 8, 9]]
 
-        assert not sudoku.is_solved(grid)
+        self.assertFalse(sudoku.is_solved(grid))
 
     def test_row_not_solved(self):
         """
@@ -52,7 +52,7 @@ class TestSudoku:
                 [7, 7, 7, 7, 7, 7, 7, 7, 7],
                 [8, 8, 8, 8, 8, 8, 8, 8, 8],
                 [9, 9, 9, 9, 9, 9, 9, 9, 9]]
-        assert not sudoku.is_solved(grid)
+        self.assertFalse(sudoku.is_solved(grid))
 
     def test_incomplete_solution(self):
         """
@@ -68,4 +68,101 @@ class TestSudoku:
                 [9, 6, 8, 2, 3, 1, 5, 7, 4],
                 [None, 7, 4, 9, 6, 8, 2, 3, 1]]
 
-        assert not sudoku.is_solved(grid)
+        self.assertFalse(sudoku.is_solved(grid))
+
+    def test_csp_search(self):
+        grid = [[None, 2, 3, 4, 5, 6, 7, 8, 9],
+                [7,    8, 9, 1, 2, 3, 4, 5, 6],
+                [4,    5, 6, 7, 8, 9, 1, 2, 3],
+                [3,    1, 2, 8, 4, 5, 9, 6, 7],
+                [6,    9, 7, 3, 1, 2, 8, 4, 5],
+                [8,    4, 5, 6, 9, 7, 3, 1, 2],
+                [2,    3, 1, 5, 7, 4, 6, 9, 8],
+                [9,    6, 8, 2, 3, 1, 5, 7, 4],
+                [5,    7, 4, 9, 6, 8, 2, 3, 1]]
+        expected_values = {(0, 0, 1)}
+        for assignment in sudoku.csp_search(grid):
+            self.assertTrue(assignment in expected_values)
+            expected_values.remove(assignment)
+
+    def test_csp_search2(self):
+        grid = [[None, 2, 3, 4,    5, 6, 7, 8, 9],
+                [7,    8, 9, 1,    2, 3, 4, 5, 6],
+                [4,    5, 6, 7,    8, 9, 1, 2, 3],
+                [3,    1, 2, 8,    4, 5, 9, 6, 7],
+                [6,    9, 7, None, 1, 2, 8, 4, 5],
+                [8,    4, 5, 6,    9, 7, 3, 1, 2],
+                [2,    3, 1, 5,    7, 4, 6, 9, 8],
+                [9,    6, 8, 2,    3, 1, 5, 7, 4],
+                [5,    7, 4, 9,    6, 8, 2, 3, None]]
+        expected_values = {(0, 0, 1), (8, 8, 1), (4, 3, 3)}
+        for assignment in sudoku.csp_search(grid):
+            self.assertTrue(assignment in expected_values)
+            expected_values.remove(assignment)
+
+    def test_csp_search3(self):
+        grid = [[None, None, None, None, None, None, None, 6,    None],
+                [None, None, 1,    None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, 6,    None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [6,    None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None]]
+        expected_values = {(1, 1, 6)}
+        assignment = next(sudoku.csp_search(grid))
+        self.assertTrue(assignment in expected_values)
+
+    def test_bad_csp_search(self):
+        grid = [[None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None]]
+        # Python generators raise a StopIteration exception when they return.
+        self.assertRaises(StopIteration, next, sudoku.csp_search(grid))
+
+    def test_easy_solve(self):
+        # This is an "easy" sudoku problem pulled from the internet
+        grid = [[None, None, 3, None, 7, 4, None, 1, None],
+                [8, None, None, 5, 6, 9, None, 4, None],
+                [6, None, None, None, 2, 1, None, None, 8],
+                [None, 1, None, None, 4, 7, 9, None, None],
+                [2, None, None, None, None, None, None, None, 3],
+                [None, None, 4, 2, 3, None, None, 6, None],
+                [1, None, None, 7, 9, None, None, None, 4],
+                [None, 7, None, 4, 8, 6, None, None, 2],
+                [None, 3, None, 1, 5, None, 7, None, None]]
+        solved_grid = sudoku.solve(grid)
+        self.assertTrue(sudoku.is_solved(solved_grid),
+                        "\n" + sudoku.grid_str(solved_grid))
+
+    @unittest.skip("Not ready for this one yet")
+    def test_hard_solve(self):
+        grid = [[None, None, None, None, 6,    None, None, None, 3],
+                [None, None, None, None, None, None, None, 4,    2],
+                [None, None, 3,    9,    2,    None, 7,    None, None],
+                [4,    None, None, 3,    None, None, None, 7,    None],
+                [None, None, 6,    None, None, None, 8,    None, None],
+                [None, 2,    None, None, None, 7,    None, None, 9],
+                [None, None, 9,    None, 5,    2,    3,    None, None],
+                [6,    8,    None, None, None, None, None, None, None],
+                [7,    None, None, None, 1,    None, None, None, None]]
+        # solution = [[2, 5, 4, 7, 6, 1, 9, 8, 3],
+        #             [9, 6, 7, 5, 3, 8, 1, 4, 2],
+        #             [8, 1, 3, 9, 2, 4, 7, 5, 6],
+        #             [4, 9, 1, 3, 8, 6, 2, 7, 5],
+        #             [3, 7, 6, 2, 9, 5, 8, 1, 4],
+        #             [5, 2, 8, 1, 4, 7, 6, 3, 9],
+        #             [1, 4, 9, 8, 5, 2, 3, 6, 7],
+        #             [6, 8, 2, 4, 7, 3, 5, 9, 1],
+        #             [7, 3, 5, 6, 1, 9, 4, 2, 8]]
+        solved_grid = sudoku.solve(grid)
+        print(sudoku.grid_str(solved_grid))
+        self.assertTrue(sudoku.is_solved(solved_grid),
+                        "\n" + sudoku.grid_str(solved_grid))
